3bd7de147ff362f5f7c278fec69e29191bd24ea9
AyRick <aymeric.daniel@isen.yncrea.fr>
2023-11-21 14:52:00 +0100

    Debug 1

--- pages/components/Sections/Banner.tsx
+++ pages/components/Sections/Banner.tsx
@@ -12,25 +12,40 @@ interface BannerProps {
     alt?: string;
 }
 
-const Banner: React.FC<BannerProps> = ({image, overlineText, headlineText, text, buttonText, buttonHref, alt}) => (
-    <div className="banner">
-        <div className="banner-image">
-            <img src={image} alt={alt}/>
-        </div>
-        <div className="H1-preset-middle">
-            <div className="H1-preset-text-middle">
-                <div className="header-group">
-                    <div className="overline">{overlineText}</div>
-                    <div className="headline">{headlineText}</div>
-                </div>
-                <div className="paragraph">{text}</div>
-            </div>
-            <Link href={buttonHref} className="button">
+const Banner: React.FC<BannerProps> = ({ image, overlineText, headlineText, text, buttonText, buttonHref, alt }) => {
+    const isInternalLink = buttonHref.startsWith('/');
+
+    const LinkButton = isInternalLink ? (
+        <Link href={buttonHref}>
+            <a className="button">
                 <div className="button-text">{buttonText}</div>
                 <Chevron/>
-            </Link>
+            </a>
+        </Link>
+    ) : (
+        <a href={buttonHref} className="button" target="_blank" rel="noopener noreferrer">
+            <div className="button-text">{buttonText}</div>
+            <Chevron/>
+        </a>
+    );
+
+    return (
+        <div className="banner">
+            <div className="banner-image">
+                <img src={image} alt={alt}/>
+            </div>
+            <div className="H1-preset-middle">
+                <div className="H1-preset-text-middle">
+                    <div className="header-group">
+                        <div className="overline">{overlineText}</div>
+                        <div className="headline">{headlineText}</div>
+                    </div>
+                    <div className="paragraph">{text}</div>
+                </div>
+                {LinkButton}
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Banner;
