b9188dd591ae32be70bdb78e9dd06b90bd638774
AyRick <aymeric.daniel@isen.yncrea.fr>
2023-11-27 14:51:38 +0100

    Debug br keys

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import Hero from './components/Sections/Hero';
 import Banner from './components/Sections/Banner';
 import IconFeatures from './components/Sections/IconFeatures';
 import MediaFeature from './components/Sections/MediaFeature';
+import {KEYS} from "eslint-visitor-keys";
 
 const HomePage: React.FC = () => {
         return (
@@ -37,7 +38,7 @@ const HomePage: React.FC = () => {
                     overlineText="3 septembre 2023"
                     headlineText="5ème édition"
                     text={["La grande course de la rentrée revient avec l'ambition de rassembler le plus grand monde possible.",
-                        <br/>,
+                        <br key="break"/>,
                         "Cette année, nous espérons accueillir des malvoyants qui courent en binôme avec leur guide !"
                     ]}
                     buttonText="s’inscrire"
@@ -51,7 +52,7 @@ const HomePage: React.FC = () => {
                     overlineText="Une autre façon de participer"
                     headlineText="Devenez bénévoles"
                     text={["Afin d’assurer une organisation sans failles, nous avons besoin de 100 bénévoles.",
-                        <br/>,
+                        <br key="break"/>,
                         "Contactez nous au 06 85 93 41 12."
                     ]}
                     buttonText="Nous contacter"
