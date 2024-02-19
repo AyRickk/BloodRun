3b4109fc9ad9b27da2c0bdbf0445d38fc4e1ceb9
AyRick <aymeric.daniel@isen.yncrea.fr>
2024-02-19 10:36:15 +0100

    speed insight + audit

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
 import 'leaflet/dist/leaflet.css';
 import 'react-leaflet-markercluster/dist/styles.min.css';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
